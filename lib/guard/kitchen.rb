d9a5ac576ad520cef25ed01aa4c968fba91a9ad6
adamhjk <adam@opscode.com>
2013-05-10 14:11:30 -0700

    Initial commit

---
+++ lib/guard/kitchen.rb
@@ -0,0 +1,101 @@
+require "guard/kitchen/version"
+require "guard"
+require "guard/guard"
+require "mixlib/shellout"
+
+module Guard
+  class Kitchen < Guard
+    def start
+      ::Guard::UI.info("Guard::Kitchen is starting")
+      cmd = Mixlib::ShellOut.new("kitchen create")
+      cmd.live_stream = STDOUT
+      cmd.run_command
+      begin
+        cmd.error!
+        Notifier.notify('Kitchen created', :title => 'test-kitchen', :image => :success)
+      rescue Mixlib::ShellOut::ShellCommandFailed => e
+        Notifier.notify('Kitchen create failed', :title => 'test-kitchen', :image => :failed)
+        ::Guard::UI.info("Kitchen failed with #{e.to_s}")
+        throw :task_has_failed
+      end
+    end
+
+    def stop
+      ::Guard::UI.warning("Guard::Kitchen cannot stop for you, due to strange bug.")
+      ::Guard::UI.warning("You likely want to run 'kitchen destroy'")
+      #::Guard::UI.info("Guard::Kitchen is stopping")
+      #cmd = Mixlib::ShellOut.new("kitchen destroy")
+      #cmd.live_stream = STDOUT
+      #cmd.run_command
+      #begin
+      #  cmd.error!
+      #rescue Mixlib::ShellOut::ShellCommandFailed => e
+      #  ::Guard::UI.info("Kitchen failed with #{e.to_s}")
+      #  throw :task_has_failed
+      #ensure
+      #  # Sometimes, we leave the occasional shell process unreaped!
+      #  Process.waitall
+      #end
+    end
+
+    def reload
+      stop
+      start
+    end
+
+    def run_all
+      ::Guard::UI.info("Guard::Kitchen is running all tests")
+      cmd = Mixlib::ShellOut.new("kitchen verify")
+      cmd.live_stream = STDOUT
+      cmd.run_command
+      begin
+        cmd.error!
+        Notifier.notify('Kitchen verify succeeded', :title => 'test-kitchen', :image => :success)
+        ::Guard::UI.info("Kitchen verify succeeded")
+      rescue Mixlib::ShellOut::ShellCommandFailed => e
+        Notifier.notify('Kitchen verify failed', :title => 'test-kitchen', :image => :failed)
+        ::Guard::UI.info("Kitchen verify failed with #{e.to_s}")
+        throw :task_has_failed
+      end
+    end
+
+    def run_on_changes(paths)
+      suites = {}
+      paths.each do |path|
+        if path =~ /test\/integration\/(.+?)\/.+/
+          suites[$1] = true
+        end
+      end
+      if suites.length > 0
+        ::Guard::UI.info("Guard::Kitchen is running suites: #{suites.keys.join(' ')}")
+        cmd = Mixlib::ShellOut.new("kitchen verify '(#{suites.keys.join('|')})-.+' -p")
+        cmd.live_stream = STDOUT
+        cmd.run_command
+        begin
+          cmd.error!
+          Notifier.notify("Kitchen verify succeeded for: #{suites.keys.join(', ')}", :title => 'test-kitchen', :image => :success)
+          ::Guard::UI.info("Kitchen verify succeeded for: #{suites.keys.join(', ')}")
+        rescue Mixlib::ShellOut::ShellCommandFailed => e
+          Notifier.notify("Kitchen verify failed for: #{suites.keys.join(', ')}", :title => 'test-kitchen', :image => :failed)
+          ::Guard::UI.info("Kitchen verify failed with #{e.to_s}")
+          throw :task_has_failed
+        end
+      else
+        ::Guard::UI.info("Guard::Kitchen is running converge for all suites")
+        cmd = Mixlib::ShellOut.new("kitchen converge")
+        cmd.live_stream = STDOUT
+        cmd.run_command
+        begin
+          cmd.error!
+          Notifier.notify("Kitchen converge succeeded", :title => 'test-kitchen', :image => :success)
+          ::Guard::UI.info("Kitchen converge succeeded")
+        rescue Mixlib::ShellOut::ShellCommandFailed => e
+          Notifier.notify("Kitchen converge failed", :title => 'test-kitchen', :image => :failed)
+          ::Guard::UI.info("Kitchen converge failed with #{e.to_s}")
+          throw :task_has_failed
+        end
+
+      end
+    end
+  end
+end
---
+++ lib/guard/kitchen/templates/Guardfile
@@ -0,0 +1,9 @@
+guard 'kitchen' do
+  watch(%r{test/.+})
+  watch(%r{^recipes/(.+)\.rb$})
+  watch(%r{^attributes/(.+)\.rb$})
+  watch(%r{^files/(.+)})
+  watch(%r{^templates/(.+)})
+  watch(%r{^providers/(.+)\.rb})
+  watch(%r{^resources/(.+)\.rb})
+end
