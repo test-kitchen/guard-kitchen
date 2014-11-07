1794fbe24c7357ccb2462e3a6e2f0e2aa6defd46
Greg Symons <gsymons@drillinginfo.com>
2014-11-07 11:25:09 -0600

    Explicitly copy LC_ALL into the environment

--- lib/guard/kitchen.rb
+++ lib/guard/kitchen.rb
@@ -22,7 +22,7 @@ module Guard
   class Kitchen < Guard
     def start
       ::Guard::UI.info("Guard::Kitchen is starting")
-      cmd = Mixlib::ShellOut.new("kitchen create", :timeout => 10800)
+      cmd = Mixlib::ShellOut.new("kitchen create", :timeout => 10800, :env => { 'LC_ALL' => ENV['LC_ALL'] })
       cmd.live_stream = STDOUT
       cmd.run_command
       begin
@@ -37,7 +37,7 @@ module Guard
 
     def stop
       ::Guard::UI.info("Guard::Kitchen is stopping")
-      cmd = Mixlib::ShellOut.new("kitchen destroy", :timeout => 10800)
+      cmd = Mixlib::ShellOut.new("kitchen destroy", :timeout => 10800, :env => { 'LC_ALL' => ENV['LC_ALL'] })
       cmd.live_stream = STDOUT
       cmd.run_command
       begin
@@ -57,7 +57,7 @@ module Guard
 
     def run_all
       ::Guard::UI.info("Guard::Kitchen is running all tests")
-      cmd = Mixlib::ShellOut.new("kitchen verify", :timeout => 10800)
+      cmd = Mixlib::ShellOut.new("kitchen verify", :timeout => 10800, :env => { 'LC_ALL' => ENV['LC_ALL'] })
       cmd.live_stream = STDOUT
       cmd.run_command
       begin
@@ -80,7 +80,7 @@ module Guard
       end
       if suites.length > 0
         ::Guard::UI.info("Guard::Kitchen is running suites: #{suites.keys.join(', ')}")
-        cmd = Mixlib::ShellOut.new("kitchen verify '(#{suites.keys.join('|')})-.+' -p", :timeout => 10800)
+        cmd = Mixlib::ShellOut.new("kitchen verify '(#{suites.keys.join('|')})-.+' -p", :timeout => 10800, :env => { 'LC_ALL' => ENV['LC_ALL'] })
         cmd.live_stream = STDOUT
         cmd.run_command
         begin
@@ -94,7 +94,7 @@ module Guard
         end
       else
         ::Guard::UI.info("Guard::Kitchen is running converge for all suites")
-        cmd = Mixlib::ShellOut.new("kitchen converge", :timeout => 10800)
+        cmd = Mixlib::ShellOut.new("kitchen converge", :timeout => 10800, :env => { 'LC_ALL' => ENV['LC_ALL'] })
         cmd.live_stream = STDOUT
         cmd.run_command
         begin
